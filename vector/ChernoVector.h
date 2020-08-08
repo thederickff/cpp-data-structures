d3fe029aa151e984013ece52f5dd9706d17d401d
derickfelix <derickfelix4321@gmail.com>
2020-08-08 12:11:56 -0400

    refactor MyVector

---
+++ vector/ChernoVector.h
@@ -0,0 +1,84 @@
+#ifndef __DataStructures_ChernoVector__
+#define __DataStructures_ChernoVector__
+
+#include <iostream>
+
+template<typename T>
+class ChernoVector
+{
+public:
+  ChernoVector()
+  {
+    ReAlloc(2);
+  }
+
+  ~ChernoVector()
+  {
+    delete[] m_Data;
+  }
+
+  void PushBack(const T &value)
+  {
+    if (m_Size >= m_Capacity) {
+      ReAlloc(m_Capacity * 1.5);
+    }
+
+    m_Data[m_Size] = value;
+    m_Size++;
+  }
+
+  void PushBack(const T &&value)
+  {
+    if (m_Size >= m_Capacity) {
+      ReAlloc(m_Capacity * 1.5);
+    }
+
+    m_Data[m_Size] = std::move(value);
+    m_Size++;
+  }
+
+  template<typename... Args>
+  T& EmplaceBack(Args&&... args)
+  {
+    if (m_Size >= m_Capacity) {
+      ReAlloc(m_Capacity * 1.5);
+    }
+
+    m_Data[m_Size] = T(std::forward<Args>(args)...);
+    m_Size++;
+  }
+
+  constexpr size_t Size() const { return m_Size; }
+
+  T &operator[](size_t index) { return m_Data[index]; }
+
+  const T &operator[](size_t index) const { return m_Data[index]; }
+
+private:
+  void ReAlloc(size_t newCapacity)
+  {
+    T* newBlock = new T[newCapacity];
+
+    if (newCapacity < m_Size) {
+      m_Size = newCapacity;
+    }
+
+    for (size_t i = 0; i < m_Size; ++i) {
+      newBlock[i] = std::move(m_Data[i]);
+    }
+
+    delete[] m_Data;
+    
+    std::cout << "Growing from " << m_Capacity << " to " << newCapacity << std::endl;
+    m_Data = newBlock;
+    m_Capacity = newCapacity;
+  }
+
+private:
+  T *m_Data = nullptr;
+  
+  size_t m_Size = 0;
+  size_t m_Capacity = 0;
+};
+
+#endif /* defined(__DataStructures_ChernoVector__) */
--- vector/main.cpp
+++ vector/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 
 #include "MyVector.h"
+#include "ChernoVector.h"
+
+struct Vector3
+{
+  float x = 0.0f, y = 0.0f, z = 0.0f;
+
+  Vector3() {}
+
+  Vector3(float scalar)
+  : x(scalar), y(scalar), z(scalar) { }
+
+  Vector3(float x, float y, float z)
+  : x(x), y(y), z(z) { }
+
+  Vector3(const Vector3 &other)
+  : x(other.x), y(other.y), z(other.z)
+  {
+    std::cout << "Copy\n";
+  }
+
+  Vector3(const Vector3 &&other)
+  : x(other.x), y(other.y), z(other.z)
+  {
+    std::cout << "Move\n";
+  }
+
+  ~Vector3()
+  {
+    std::cout << "Destroy\n";
+  }
+
+  Vector3 &operator=(const Vector3 &other)
+  {
+    std::cout << "Copy\n";
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    return *this;
+  }
+
+  Vector3 &operator=(const Vector3 &&other)
+  {
+    std::cout << "Move\n";
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    return *this;
+  }
+};
 
 void MyVectorTest()
 {
@@ -29,9 +80,39 @@ void MyVectorTest()
   }
 }
 
+void ChernoVectorTest()
+{
+  std::cout << "\nChernoVector: std::string" << std::endl;
+  ChernoVector<std::string> vector;
+  vector.PushBack("Cherno");
+  vector.PushBack("C++");
+  vector.PushBack("Vector");
+
+  for (size_t i = 0; i < vector.Size(); ++i) {
+    std::cout << vector[i] << std::endl;
+  }
+
+  std::cout << "----------------------" << std::endl;
+  std::cout << "\nChernoVector: Vector3" << std::endl;
+  ChernoVector<Vector3> vector3s;
+  // vector3s.PushBack(Vector3(30.0f));
+  // vector3s.PushBack(Vector3(1.0, 1.5, 0.5));
+  // vector3s.PushBack(Vector3());
+
+  vector3s.EmplaceBack(30.0f);
+  vector3s.EmplaceBack(1.0, 1.5, 0.5);
+  vector3s.EmplaceBack();
+
+  for (size_t i = 0; i < vector3s.Size(); ++i) {
+    std::cout << "{ " << vector3s[i].x << ", " << vector3s[i].y << ", " << vector3s[i].z << " }" << std::endl;
+  }
+}
+
+
 int main()
 {
   MyVectorTest();
+  ChernoVectorTest();
 
   return 0;
 }
